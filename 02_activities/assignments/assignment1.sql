be6f2b0dc5d91636cad80c0199c8cda771fd5c50
Sanjana Garg <sanjanabansal1994@gmail.com>
2024-12-15 10:57:01 -0500

    Adding more sql code for assignment-one section 2

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -34,7 +34,9 @@ filtered by vendor IDs between 8 and 10 (inclusive) using either:
 	2.  one condition using BETWEEN
 */
 -- option 1
-
+SELECT *, quantity*cost_to_customer_per_qty as price
+FROM customer_purchases
+WHERE vendor_id >=8 AND vendor_id <=10;
 
 -- option 2
 SELECT *, quantity*cost_to_customer_per_qty as price
